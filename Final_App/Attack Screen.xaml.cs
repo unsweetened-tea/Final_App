43e86d129c7979aaca017067565b4f146a083d55
Crackedform <153736863+Crackedform@users.noreply.github.com>
2023-12-19 13:01:03 -0500

    Add files via upload

---
+++ Final_App/Attack Screen.xaml.cs
@@ -0,0 +1,9 @@
+namespace Final_App;
+
+public partial class Attack_Screen : ContentPage
+{
+	public Attack_Screen()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Final_App/Enemy1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_App
+{
+    internal class Enemy1 : Enemy
+    {
+        private string ImageUrl { get; set; }
+        private int HP { get; set; }
+        private int MAXHP { get; set; }
+        private int xPos { get; set; }
+        private bool Flying { get; set; }
+        private bool WearingHat { get; set; }
+        private int percentHealth { get; set; }
+        private int dmg { get; set; }
+        private bool LongRange { get; set; }
+        public Enemy1(int xpos)
+        {
+            HP = 10;
+            MAXHP = 10;
+            xPos = xpos;
+            Flying = false;
+            WearingHat = false;
+            dmg = 1;
+            LongRange = false;
+        }
+        public override void Attack(Enemy enemy)
+        {
+
+        }
+        public override void Move(int x)
+        {
+
+        }
+
+        public override void Run(Enemy enemy, int percentHealth)
+        {
+
+        }
+
+        public override void update(Enemy enemy)
+        {
+
+        }
+    }
+}
