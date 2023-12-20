da4cbc52468c36829c4ffac1ec6463301b09d94b
MeghanadhV <75938676+MeghanadhV@users.noreply.github.com>
2023-12-20 10:01:27 -0500

    Update WeaponsMenu.xaml.cs

--- Final_App/WeaponsMenu.xaml.cs
+++ Final_App/WeaponsMenu.xaml.cs
@@ -1,9 +1,29 @@
-namespace Final_App;
+using System;
+using System.Windows;
+using System.Windows.Controls;
 
-public partial class WeaponsMenu : ContentPage
+
+namespace FinalProject;
+
+public partial class WeaponSelection : ContentPage
 {
-	public WeaponsMenu()
+	public WeaponSelection()
 	{
 		InitializeComponent();
 	}
+
+    private void WeaponButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Add logic for handling weapon button clicks here
+        // For example:
+        Button clickedButton = (Button)sender;
+        MessageBox.Show($"You clicked on {clickedButton.Name}!");
+    }
+
+    // Event handler for Buy button click
+    private void BuyButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Add logic for handling Buy button click here
+        MessageBox.Show("Buy button clicked!");
+    }
 }
