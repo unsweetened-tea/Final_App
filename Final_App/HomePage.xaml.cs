baa12dfc073e7cf63f8d63c06bd3064d3ccaf9b6
Crackedform <153736863+Crackedform@users.noreply.github.com>
2023-12-21 22:12:35 -0500

    Update HomePage.xaml.cs
    Taken from my branch

--- Final_App/HomePage.xaml.cs
+++ Final_App/HomePage.xaml.cs
@@ -1,11 +1,13 @@
-namespace Final_App;
+using System.Net.Sockets;
 
+namespace Final_App;
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public int selected = 0;
+    public HomePage()
+    {
+        InitializeComponent();
+    }
 
     private void start_Clicked(object sender, EventArgs e)
     {
@@ -29,5 +31,53 @@ public partial class HomePage : ContentPage
     {
         //var page = new SkinsMenu();
         //Navigation.PushAsync(page)
+    }
+    void Level_Clicked(System.Object sender, System.EventArgs e)
+    {
+        var button = sender as ImageButton;
+        if (button == null)
+            return;
+        else if(selected == 1)
+        {
+            fam.TextColor = Colors.White;
+        }
+        else if (selected == 2)
+        {
+            fod.TextColor = Colors.White;
+        }
+        else if (selected == 3)
+        {
+            fav.TextColor = Colors.White;
+        }
+        else if (selected == 4)
+        {
+            trav.TextColor = Colors.White;
+        }
+        else if (selected == 5)
+        {
+            gret.TextColor = Colors.White;
+        }
+
+        else if (button.Source.Equals("Family.png"))
+        {
+            fam.TextColor = Colors.Yellow;
+            selected = 1;
+        }
+        else if (button.Source.Equals("food.png"))
+        {
+            fod.TextColor = Colors.Yellow;
+        }
+        else if (button.Source.Equals("likedislike.png"))
+        {
+            fav.TextColor = Colors.Yellow;
+        }
+        else if (button.Source.Equals("travel.png"))
+        {
+            trav.TextColor = Colors.Yellow;
+        }
+        else if (button.Source.Equals("basics.png"))
+        {
+            gret.TextColor = Colors.Yellow;
+        }
     }
 }
