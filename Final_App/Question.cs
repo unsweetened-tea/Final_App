c0ca3e637a092d51c8cdf01bafdb75bba7f2bff8
leogo <leogo@MFPC>
2024-01-11 13:17:01 -0500

    added Question class

---
+++ Final_App/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_App
+{
+    internal class Question
+    {
+        private string category;
+        private string difficulty;
+        public Question() { }
+    }
+}
