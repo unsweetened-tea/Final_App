d472ed52788ec9d191acdf7a07197e0784febc60
G1-100 <66092794+G1-100@users.noreply.github.com>
2024-01-13 15:48:01 -0500

    Added passing data between home and skin/weapon selection pages - still need to add database

--- Final_App/HomePage.xaml.cs
+++ Final_App/HomePage.xaml.cs
@@ -3,17 +3,127 @@ using System.Collections.ObjectModel;
 using System.Net.Sockets;
 
 namespace Final_App;
+
+public class WeaponsArgs : EventArgs
+{
+    public int coins;
+    public ObservableCollection<Weapon> weaponsUsed;
+    public ObservableCollection<Weapon> weapons;
+}
+
+public class SkinsArgs : EventArgs
+{
+    public int coins;
+    public Skin skinUsed;
+    public ObservableCollection<Skin> skins;
+}
+
 public partial class HomePage : ContentPage
 {
     Label selected;
+    int coins;
+    ObservableCollection<Weapon> weaponsUsed;
+    ObservableCollection<Weapon> weapons;
+    ObservableCollection<Skin> skins;
+    Skin curSkin;
+
     public HomePage()
     {
         InitializeComponent();
         selected = fam;
         fam.TextColor = Colors.LightBlue;
-        Preferences.Default.Set("skinsUsed", new ObservableCollection<Skin>());
-        Preferences.Default.Set("weaponsUsed", new ObservableCollection<Weapon>());
+        coins = 10000;
+        skins = new ObservableCollection<Skin>()
+        {
+            new Skin()
+            {
+                Name = "Boy",
+                ImageURL = "boy.png",
+                Cost = 0,
+                Bought = true,
+            },
+            new Skin()
+            {
+                Name = "Dino",
+                ImageURL = "dino.png",
+                Cost = 100,
+            },
+            new Skin()
+            {
+                Name = "Knight",
+                ImageURL = "knight.png",
+                Cost = 50,
+            },
+            new Skin()
+            {
+                Name = "Red Hat",
+                ImageURL = "redhat.png",
+                Cost = 350
+            },
+            new Skin()
+            {
+                Name = "Zombie",
+                ImageURL = "zombie.png",
+                Cost = 10
+            },
+            new Skin()
+            {
+                Name = "Pumpkin",
+                ImageURL = "pumpkin.png",
+                Cost = 100
+            }
+        };
 
+        weapons = new ObservableCollection<Weapon>()
+        {
+            new Weapon()
+            {
+                Name = "AR-15",
+                ImageURL = "weapon1.png",
+                Cost = 200,
+                Damage = 10,
+                Difficulty = 1,
+            },
+            new Weapon()
+            {
+                Name = "Deagle",
+                ImageURL = "weapon2.png",
+                Difficulty = 0,
+                Damage = 5,
+                Cost = 100,
+            },
+            new Weapon()
+            {
+                Name = "Glock",
+                ImageURL = "weapon3.png",
+                Difficulty = 0,
+                Damage = 2,
+                Cost = 50,
+            },
+            new Weapon()
+            {
+                Name = "AK-47",
+                ImageURL = "weapon4.png",
+                Difficulty = 1,
+                Damage = 15,
+                Cost = 350
+            },
+            new Weapon()
+            {
+                Name = "Squirt",
+                ImageURL = "weapon5.png",
+                Damage = 1,
+                Cost = 10
+            },
+            new Weapon()
+            {
+                Name = "Uzi",
+                ImageURL = "weapon6.png",
+                Damage = 8,
+                Difficulty = 0,
+                Cost = 100
+            }
+        };
     }
 
     private void start_Clicked(object sender, EventArgs e)
@@ -30,13 +140,15 @@ public partial class HomePage : ContentPage
 
     void weaponButton_Clicked(System.Object sender, System.EventArgs e)
     {
-        var page = new WeaponsMenu();
+        var page = new WeaponsMenu(coins, weapons);
+        page.NavigateAway += onWeaponNavigatAway;
         Navigation.PushAsync(page);
     }
 
     void skinsButton_Clicked(System.Object sender, System.EventArgs e)
     {
-        var page = new SkinsMenu();
+        var page = new SkinsMenu(coins, skins);
+        page.NavigateAway += onSkinNavigatAway;
         Navigation.PushAsync(page);
     }
     void Level_Clicked(System.Object sender, System.EventArgs e)
@@ -76,6 +188,20 @@ public partial class HomePage : ContentPage
             selected.TextColor = Colors.Black;
             selected = gret;
         }
+    }
+
+    void onWeaponNavigatAway(object sender, WeaponsArgs e)
+    {
+        coins = e.coins;
+        weaponsUsed = e.weaponsUsed;
+        weapons = e.weapons;
+    }
+
+    void onSkinNavigatAway(object sender, SkinsArgs e)
+    {
+        coins = e.coins;
+        curSkin = e.skinUsed;
+        skins = e.skins;
     }
 
 }
--- Final_App/Skin.cs
+++ Final_App/Skin.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Final_App
 {
-    internal class Skin
+    public class Skin
     {
         public string Name;
         public int Cost;
-        public String ImageURL {get; set; }
+        public String ImageURL { get; set; }
         public bool Bought;
 
     }
